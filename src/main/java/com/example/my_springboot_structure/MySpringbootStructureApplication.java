0359d960abc099b48bfaa82f6187fd14e653590c
이동철 <choise154@gmail.com>
2023-08-01 01:27:44 +0900

    first commit

---
+++ src/main/java/com/example/my_springboot_structure/MySpringbootStructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_springboot_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_springboot_structure/MySpringbootStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_springboot_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
