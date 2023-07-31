74647805dda083aae7a3fa4f488328b415a5575f
이동철 <choise154@gmail.com>
2023-08-01 01:55:01 +0900

    config 추가

---
+++ src/main/java/com/example/my_springboot_structure/MySpringBootStructureConfig.java
@@ -0,0 +1,16 @@
+package com.example.my_springboot_structure;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySpringBootStructureConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+
+}
