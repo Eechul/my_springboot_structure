f9001910faa9a43a2bbb4f218a9bf2c02716280a
이동철 <choise154@gmail.com>
2023-09-03 14:07:38 +0900

    UserRestController 수정

--- src/main/java/com/example/my_springboot_structure/domain/user/controller/UserRestController.java
+++ src/main/java/com/example/my_springboot_structure/domain/user/controller/UserRestController.java
@@ -1,4 +1,9 @@
 package com.example.my_springboot_structure.domain.user.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
 public class UserRestController {
+
+
 }
