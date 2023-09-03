6dae90399b0da5238a5faa1be54f439a1b402fb5
이동철 <choise154@gmail.com>
2023-09-03 14:07:27 +0900

    UserRepository 작성

--- src/main/java/com/example/my_springboot_structure/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/my_springboot_structure/domain/user/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.my_springboot_structure.domain.user.repository;
 
+import com.example.my_springboot_structure.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
-
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
